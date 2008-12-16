ff2196c7d89b70fb178f68e2da91048e856e9442
Tim Moore <timoore@redhat.com>
2008-12-16 23:55:00 +0100

    Add va_end() call in testOverlayString and testDrawString

--- examples/test.c
+++ examples/test.c
@@ -135,8 +135,11 @@ void testOverlayString(const char *format, ...)
   
   va_start(ap, format);
   len = vsnprintf(NULL, 0, format, ap);
+  va_end(ap);
   overtext = (char*)malloc(len+1);
-  if (!overtext) {va_end(ap); return;}
+  if (!overtext)
+    return;
+  va_start(ap, format);
   vsnprintf(overtext, len+1, format, ap);
   va_end(ap);
 }
@@ -153,8 +156,11 @@ void testDrawString(float x, float y, const char *format, ...)
   
   va_start(ap, format);
   len = vsnprintf(NULL, 0, format, ap);
+  va_end(ap);
   text = (char*)malloc(len+1);
-  if (!text) {va_end(ap); return;}
+  if (!text)
+    return;
+  va_start(ap, format);
   vsnprintf(text, len+1, format, ap);
   va_end(ap);
   
