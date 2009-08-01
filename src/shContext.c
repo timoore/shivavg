45081fa1dea5afec34032810a509ba7752d6daa0
Mitchell Riley <mitchell.v.riley@gmail.com>
2009-08-01 14:27:49 +1000

    Get rid of a GLU reference in shContext. (Hello _why!)

--- src/shContext.c
+++ src/shContext.c
@@ -52,7 +52,7 @@ VG_API_CALL VGboolean vgCreateContextSH(VGint width, VGint height)
   
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluOrtho2D(0,width,0,height);
+  glOrtho(0,width,0,height,-1,1);
   
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
@@ -73,7 +73,7 @@ VG_API_CALL void vgResizeSurfaceSH(VGint width, VGint height)
   
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluOrtho2D(0,width,0,height);
+  glOrtho(0,width,0,height,-1,1);
   
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
